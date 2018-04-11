20f32d625ac71d92de9a9b9d6a8c2e2715afdb2c
Wessel Kuipers <wesselkuipers@gmail.com>
2018-04-12 00:39:23 +0200

    Updated UserController update methods to accept JSON Added Dao methods for updating website and displayname

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -5,23 +5,27 @@ import domain.User;
 import java.util.List;
 
 public interface UserDao {
-    public List<User> getUsers();
+    List<User> getUsers();
 
-    public User getUser(int id);
+    User getUser(int id);
 
-    public User getUserByName(String name);
+    User getUserByName(String name);
 
-    public List<User> getFollowers(User user);
+    List<User> getFollowers(User user);
 
-    public User createUser(User u);
+    User createUser(User u);
 
-    public void followUser(User u, User toFollow);
+    void followUser(User u, User toFollow);
 
-    public void unfollowUser(User u, User toUnfollow);
+    void unfollowUser(User u, User toUnfollow);
 
-    public boolean login(String username, String password);
+    boolean login(String username, String password);
 
-    public void updateBio(User user, String bio);
+    void updateBio(User user, String bio);
 
-    public void updateLocation(User user, String location);
+    void updateLocation(User user, String location);
+
+    void updateWebsite(User user, String website);
+
+    void updateDisplayname(User user, String displayname);
 }
